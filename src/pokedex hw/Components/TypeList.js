a12a419e0488bcc04319935affa778244f9aae1d
TofuSupreme <yunishbacon@gamil.com>
2020-11-25 21:31:06 +0900

    Created types and type list in components

---
+++ src/pokedex hw/Components/TypeList.js
@@ -0,0 +1,7 @@
+import Type from './Type'
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+export default function TypeList(props) {
+  return props.type.map(t => (
+    <Type key={t.name} link={t.link} bgColor={t.bgColor} name={t.name} />
+  ))
+}
