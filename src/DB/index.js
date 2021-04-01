ed23cfe761178f6149f98df6003969b84c18223a
Tofusupreme <Yunishbacon@gmail.com>
2021-04-01 19:00:44 +0900

    Added

---
+++ src/DB/index.js
@@ -0,0 +1,376 @@
+const PokemonDB = [
+  {
+    id: 1,
+    name: 'Bulbasaur',
+    link: '/pokemon/bulbasaur',
+    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+    description:
+      'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+    type: {
+      strength: [
+        {
+          link: '/types/grass',
+          bgColor: '#9bcc50',
+          name: 'Grass'
+        },
+        {
+          link: '/types/poison',
+          bgColor: '#b97fc9',
+          name: 'Poison'
+        }
+      ],
+      weakness: [
+        {
+          link: '/types/fire',
+          bgColor: '#ed6d12',
+          name: 'Fire'
+        },
+        {
+          link: '/types/psychic',
+          bgColor: '#f366b9',
+          name: 'Psychic'
+        },
+        {
+          link: '/types/flying',
+          bgColor: '#3dc7ef',
+          name: 'Flying'
+        },
+        {
+          link: '/types/ice',
+          bgColor: '#51c4e7',
+          name: 'Ice'
+        }
+      ]
+    },
+    evolutions: [
+      {
+        id: 1,
+        name: 'Bulbasaur',
+        link: '/pokemon/bulbasaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png'
+      },
+      {
+        id: 2,
+        name: 'Ivysaur',
+        link: '/pokemon/ivysaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png'
+      },
+      {
+        id: 3,
+        name: 'Venusaur',
+        link: '/pokemon/venusaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Ivysaur',
+    link: '/pokemon/ivysaur',
+    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+    description:
+      'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+    type: {
+      strength: [
+        {
+          link: '/types/grass',
+          bgColor: '#9bcc50',
+          name: 'Grass'
+        },
+        {
+          link: '/types/poison',
+          bgColor: '#b97fc9',
+          name: 'Poison'
+        }
+      ],
+      weakness: [
+        {
+          link: '/types/fire',
+          bgColor: '#ed6d12',
+          name: 'Fire'
+        },
+        {
+          link: '/types/psychic',
+          bgColor: '#f366b9',
+          name: 'Psychic'
+        },
+        {
+          link: '/types/flying',
+          bgColor: '#3dc7ef',
+          name: 'Flying'
+        },
+        {
+          link: '/types/ice',
+          bgColor: '#51c4e7',
+          name: 'Ice'
+        }
+      ]
+    },
+    evolutions: [
+      {
+        id: 1,
+        name: 'Bulbasaur',
+        link: '/pokemon/bulbasaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png'
+      },
+      {
+        id: 2,
+        name: 'Ivysaur',
+        link: '/pokemon/ivysaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png'
+      },
+      {
+        id: 3,
+        name: 'Venusaur',
+        link: '/pokemon/venusaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Venusaur',
+    link: '/pokemon/venusaur',
+    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+    description:
+      'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+    type: {
+      strength: [
+        {
+          link: '/types/grass',
+          bgColor: '#9bcc50',
+          name: 'Grass'
+        },
+        {
+          link: '/types/poison',
+          bgColor: '#b97fc9',
+          name: 'Poison'
+        }
+      ],
+      weakness: [
+        {
+          link: '/types/fire',
+          bgColor: '#ed6d12',
+          name: 'Fire'
+        },
+        {
+          link: '/types/psychic',
+          bgColor: '#f366b9',
+          name: 'Psychic'
+        },
+        {
+          link: '/types/flying',
+          bgColor: '#3dc7ef',
+          name: 'Flying'
+        },
+        {
+          link: '/types/ice',
+          bgColor: '#51c4e7',
+          name: 'Ice'
+        }
+      ]
+    },
+    evolutions: [
+      {
+        id: 1,
+        name: 'Bulbasaur',
+        link: '/pokemon/bulbasaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png'
+      },
+      {
+        id: 2,
+        name: 'Ivysaur',
+        link: '/pokemon/ivysaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png'
+      },
+      {
+        id: 3,
+        name: 'Venusaur',
+        link: '/pokemon/venusaur',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png'
+      }
+    ]
+  },
+  {
+    id: 155,
+    name: 'Cyndaquil',
+    link: '/pokemon/cyndaquil',
+    imgSrc: 'https://cdn.bulbagarden.net/upload/9/9b/155Cyndaquil.png',
+    description:
+      'Cyndaquil is a pokemon that is usually timid by nature. It often curls into a ball when intimidated. Though rare in the wild, it can be found living on the grasslands.',
+    type: {
+      strength: [
+        {
+          link: '/types/fire',
+          bgColor: '#ed6d12',
+          name: 'Fire'
+        }
+      ],
+      weakness: [
+        {
+          link: '/types/water',
+          bgColor: '#4578ed',
+          name: 'Water'
+        },
+        {
+          link: '/types/rock',
+          bgColor: '#a48f32',
+          name: 'Rock'
+        },
+        {
+          link: '/types/ground',
+          bgColor: '#dbb54d',
+          name: 'Ground'
+        }
+      ]
+    },
+    evolutions: [
+      {
+        id: 155,
+        link: '/pokemon/cyndaquil',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/155.png',
+        name: 'Cyndaquil'
+      },
+      {
+        id: 156,
+        link: '/pokemon/quilava',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/156.png',
+        name: 'Quilava'
+      },
+      {
+        id: 157,
+        link: '/pokemon/typhlosion',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/157.png',
+        name: 'Typhlosion'
+      }
+    ]
+  },
+  {
+    id: 156,
+    name: 'Quilava',
+    link: '/pokemon/quilava',
+    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/156.png',
+    description:
+      'Quilava keeps its foes at bay with the intensity of its flames and gusts of superheated air. This Pokémon applies its outstanding nimbleness to dodge attacks even while scorching the foe with flames.',
+    type: {
+      strength: [
+        {
+          link: '/types/fire',
+          bgColor: '#ed6d12',
+          name: 'Fire'
+        }
+      ],
+      weakness: [
+        {
+          link: '/types/water',
+          bgColor: '#4578ed',
+          name: 'Water'
+        },
+        {
+          link: '/types/rock',
+          bgColor: '#a48f32',
+          name: 'Rock'
+        },
+        {
+          link: '/types/ground',
+          bgColor: '#dbb54d',
+          name: 'Ground'
+        }
+      ]
+    },
+    evolutions: [
+      {
+        id: 155,
+        link: '/pokemon/cyndaquil',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/155.png',
+        name: 'Cyndaquil'
+      },
+      {
+        id: 156,
+        link: '/pokemon/quilava',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/156.png',
+        name: 'Quilava'
+      },
+      {
+        id: 157,
+        link: '/pokemon/typhlosion',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/157.png',
+        name: 'Typhlosion'
+      }
+    ]
+  },
+  {
+    id: 157,
+    name: 'Typhlosion',
+    link: '/pokemon/typhlosion',
+    imgSrc: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/157.png',
+    description:
+      'Typhlosion obscures itself behind a shimmering heat haze that it creates using its intensely hot flames. This Pokémon creates blazing explosive blasts that burn everything to cinders.',
+    type: {
+      strength: [
+        {
+          link: '/types/fire',
+          bgColor: '#ed6d12',
+          name: 'Fire'
+        }
+      ],
+      weakness: [
+        {
+          link: '/types/water',
+          bgColor: '#4578ed',
+          name: 'Water'
+        },
+        {
+          link: '/types/rock',
+          bgColor: '#a48f32',
+          name: 'Rock'
+        },
+        {
+          link: '/types/ground',
+          bgColor: '#dbb54d',
+          name: 'Ground'
+        }
+      ]
+    },
+    evolutions: [
+      {
+        id: 155,
+        link: '/pokemon/cyndaquil',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/155.png',
+        name: 'Cyndaquil'
+      },
+      {
+        id: 156,
+        link: '/pokemon/quilava',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/156.png',
+        name: 'Quilava'
+      },
+      {
+        id: 157,
+        link: '/pokemon/typhlosion',
+        imgSrc:
+          'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/157.png',
+        name: 'Typhlosion'
+      }
+    ]
+  }
+]
+
+export default PokemonDB
---
+++ src/DB/people.js
@@ -0,0 +1,94 @@
+const PeopleDB = [
+  {
+    id: '5fcf572e8af5912c12e3f86f',
+    age: 38,
+    name: 'Flynn Dickson',
+    gender: 'male',
+    email: 'flynndickson@tetratrex.com',
+    phone: '+1 (833) 422-3314',
+    address: [
+      '454 Whitney Avenue, Noblestown, New York, 5386',
+      '496 Lincoln Road, Gallina, Rhode Island, 2885',
+      '114 Suydam Place, Brethren, Massachusetts, 3048',
+      '787 Arlington Place, Kohatk, Vermont, 5381',
+      '991 Allen Avenue, Driftwood, Wyoming, 1083'
+    ]
+  },
+  {
+    id: '5fcf572e92adf2c1834e2130',
+    age: 35,
+    name: 'Isabel Sosa',
+    gender: 'female',
+    email: 'isabelsosa@tetratrex.com',
+    phone: '+1 (872) 429-3029',
+    address: [
+      '217 Berkeley Place, Lupton, Maryland, 6795',
+      '641 Gerry Street, Chalfant, Puerto Rico, 5537',
+      '189 Montgomery Street, Waumandee, South Dakota, 5004',
+      '628 Oak Street, Canterwood, Missouri, 9631',
+      '990 Willoughby Avenue, Lafferty, Mississippi, 7467'
+    ]
+  },
+  {
+    id: '5fcf572e4df3e3ae8f4d32af',
+    age: 29,
+    name: 'May Rodgers',
+    gender: 'male',
+    email: 'mayrodgers@tetratrex.com',
+    phone: '+1 (870) 507-3288',
+    address: [
+      '796 President Street, Winchester, Delaware, 9321',
+      '213 Post Court, Jessie, New Jersey, 6320',
+      '546 Exeter Street, Chesapeake, Oregon, 2470',
+      '314 Bay Parkway, Joes, New York, 9756',
+      '658 Brooklyn Avenue, Volta, Illinois, 2796'
+    ]
+  },
+  {
+    id: '5fcf572eff3e34f4e7028632',
+    age: 39,
+    name: 'Janna Ellison',
+    gender: 'female',
+    email: 'jannaellison@tetratrex.com',
+    phone: '+1 (864) 475-3103',
+    address: [
+      '919 Colby Court, Sehili, West Virginia, 8588',
+      '143 Tech Place, Celeryville, Nevada, 3218',
+      '265 Chase Court, Corinne, Hawaii, 3105',
+      '774 Narrows Avenue, Dunbar, Michigan, 7471',
+      '269 Brighton Court, Keyport, Northern Mariana Islands, 8745'
+    ]
+  },
+  {
+    id: '5fcf572e513fc4d9c6b1497f',
+    age: 25,
+    name: 'Madeline Perkins',
+    gender: 'female',
+    email: 'madelineperkins@tetratrex.com',
+    phone: '+1 (921) 557-2480',
+    address: [
+      '5  29 Conklin Avenue, Marysville, New Mexico, 1849',
+      '408 Montague Terrace, Comptche, New York, 3139',
+      '913 Channel Avenue, Joppa, Nebraska, 1391',
+      '4  17 Maple Avenue, Beaulieu, Marshall Islands, 2182',
+      '9  32 Roder Avenue, Falconaire, Virgin Islands, 6912'
+    ]
+  },
+  {
+    id: '5fcf572eed8da5e3721dba79',
+    age: 21,
+    name: 'Blanca Martinez',
+    gender: 'female',
+    email: 'blancamartinez@tetratrex.com',
+    phone: '+1 (806) 455-3687',
+    address: [
+      '444 McKibbin Street, Mansfield, Palau, 7180',
+      '906 Kingston Avenue, Kenmar, New York, 9352',
+      '456 Cumberland Walk, Tyro, California, 4238',
+      '249 Anna Court, Churchill, Pennsylvania, 5560',
+      '288 Belvidere Street, Needmore, Alabama, 8308'
+    ]
+  }
+]
+
+export default PeopleDB
